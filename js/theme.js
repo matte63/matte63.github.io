d918ad8c4101f08aabc6bee9df70b509c814c53b
matte63 <matte63@users.noreply.github.com>
2021-05-05 07:40:44 +0200

    updated theme function for localStorage (removed switch)

--- js/theme.js
+++ js/theme.js
@@ -1,24 +1,18 @@
 document.body.onload = function() {
+  let theme_saved = localStorage.getItem('theme');
+  if (theme_saved != null)
+    document.body.setAttribute('theme', theme_saved);
   let theme = document.createElement('button');
   theme.innerHTML = 'bubble_chart';
   theme.classList.add('icon');
+  theme.style = 'position:fixed;bottom:16px;right:16px;';
   theme.onclick = function() {
-    switch (document.body.getAttribute('theme')) {
-      case 'light':
-        document.body.setAttribute('theme', 'dark');
-        break;
-      case 'dark':
-        document.body.setAttribute('theme', 'coffee');
-        break;
-      case 'coffee':
-        document.body.setAttribute('theme', 'indigo');
-        break;
-      case 'indigo':
-        document.body.setAttribute('theme', 'light');
-        break;
-      default:
-        document.body.setAttribute('theme', 'light');
-    }
+    let theme_list = ['dark', 'light', 'indigo', 'coffee'];
+    let theme_index = theme_list.indexOf(document.body.getAttribute('theme'));
+    let theme_new = theme_list[(theme_index + 1) % theme_list.length];
+    document.body.setAttribute('theme', theme_new);
+    localStorage.setItem('theme', theme_new);
+
   };
 
   document.body.appendChild(theme);
